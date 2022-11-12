ed15d743929125f26ac507cde8c822516e4ffea9
Denis <TsukanovDenis83@yandex.ru>
2022-11-12 10:17:31 +0400

    initial: task1

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+for(let i = 0; i < 2; i++) {
+    let titleOfFilm = prompt('Один из последних просмотреных фильмов?');
+    let ratingOfFilm = prompt('На сколько его оцените?');
+    personalMovieDB.movies[titleOfFilm] = ratingOfFilm;
+}
+
+
+
+console.log(personalMovieDB.movies);
